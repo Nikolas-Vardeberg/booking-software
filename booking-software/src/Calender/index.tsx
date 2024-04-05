8c449c9f0c457bc3b5fdc9d1fe6b6f4ce836ad6f
Nikolas-Vardeberg <post@nikolas.no>
2024-04-05 15:53:27 +0200

    displaying react-calendar with styling

--- booking-software/src/Calender/index.tsx
+++ booking-software/src/Calender/index.tsx
@@ -1,9 +1,19 @@
-import FC from "react"
+import {FC} from "react"
+import ReactCalendar from "react-calendar"
 
 interface indexProps {}
 
 const index: FC<indexProps> = ({}) => {
-    return <div>index</div>
+    return(
+        <div>
+            <ReactCalendar 
+                minDate={new Date()}
+                className="REACT_CALENDAR p-2"
+                view="month"
+                onClickDay={(date) => console.log(date)}
+            />
+        </div>
+    )
 }
 
 export default index
